bb568a9d1c2a957ccb0072d7438791df60c3be5e
Andreas Engedal <andreas.engedal@hotmail.com>
2018-11-28 10:28:36 +0100

    Added failing test

---
+++ UpdateParkingLotService/TestUpdateParkingLotService/UpdateParkingLotTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TestUpdateParkingLotService
+{
+    public class UpdateParkingLotTest
+    {
+        [Fact]
+        public void TestShouldFail()
+        {
+            Assert.Equal(1, 2);
+        }
+    }
+}
