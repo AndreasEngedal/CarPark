07105b24bdb7ec596f64809a392f057c81625b4a
Andreas Engedal <andreas.engedal@hotmail.com>
2018-11-28 10:16:54 +0100

    Added empty UpdateParkingLotService

---
+++ UpdateParkingLotService/UpdateParkingLotService/App_Code/Service.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class Service : IService
+{
+    public void AddCarToParkingLot(string carRegNr, int x, int y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveCarFromParkingLot(int x, int y)
+    {
+        throw new NotImplementedException();
+    }
+}
